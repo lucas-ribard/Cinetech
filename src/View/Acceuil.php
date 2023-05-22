0181739f36f9a9fe83caf53b76ec4094253098f0
lucas-ribard <lucas.ribard@laplateforme.io>
2023-05-22 15:11:41 +0200

    ADDED : HomePage as Acceuil.php

---
+++ src/View/Acceuil.php
@@ -0,0 +1,24 @@
+<?php 
+
+use App\Controller\SeriesController;
+use App\Controller\FilmsController;
+
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceuil CineTech</title>
+</head>
+<body>
+    
+</body>
+</html>
