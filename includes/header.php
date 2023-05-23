19799ab918ae0316d22d1e84e517b42a7e113df6
lucas-ribard <lucas.ribard@laplateforme.io>
2023-05-23 10:44:53 +0200

    ADDED : simple Header

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<header>
+
+    <div>
+        <h1><a href="">CineTech</a></h1>
+    </div>
+
+    <div>
+    <h1><i class="fa-solid fa-user fa-lg"></i></h1>
+    </div>
+
+</header>
--- src/View/Acceuil.php
+++ src/View/Acceuil.php
@@ -1,24 +1,35 @@
-<?php 
+<?php
 
 use App\Controller\SeriesController;
 use App\Controller\FilmsController;
-
-
-
-
-
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link href="includes/header.css" rel="stylesheet" type="text/css" />
+    <!-- FontAwesome  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Share+Tech&display=swap" rel="stylesheet">
+
     <title>Acceuil CineTech</title>
 </head>
+
 <body>
-    
+    <!-- Header -->
+    <?php require_once("includes/header.php"); ?>
+
 </body>
+
 </html>
